4cc3aba9fcd47e8fde33cd74c8f42360981193d6
Sayed Shaun <sayedshaun4@gmail.com>
2026-08-14 09:17:23 +0600

    feat: export coding tools and CodingToolbox from tools package

---
+++ subagents/tools/__init__.py
@@ -0,0 +1,11 @@
+from .coding import CodingToolbox, apply_patch, grep, read_file, run_shell, set_auto_approve, write_file
+
+__all__ = [
+    "CodingToolbox",
+    "apply_patch",
+    "grep",
+    "read_file",
+    "run_shell",
+    "set_auto_approve",
+    "write_file",
+]
