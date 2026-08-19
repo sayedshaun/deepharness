284ad6efb47115b3542813aeb0e8f57d923ff092
Sayed Shaun <sayedshaun4@gmail.com>
2026-08-19 23:02:27 +0600

    chore: import Graph from its renamed module

--- subagents/graph/__init__.py
+++ subagents/graph/__init__.py
@@ -1,5 +1,5 @@
+from .builder import Graph, NodeSpec
 from .executor import ConcurrentUpdateError, ExecutionError, Executor, StepLimitExceeded
-from .graph import Graph, NodeSpec
 from .reducers import concat, merge_dicts
 
 __all__ = [
