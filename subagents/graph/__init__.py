a9980813c9d811bcf4f47403ba156f7e8b495f63
Sayed Shaun <sayedshaun4@gmail.com>
2026-08-19 22:27:33 +0600

    chore: export reducers and graph errors

--- subagents/graph/__init__.py
+++ subagents/graph/__init__.py
@@ -1,4 +1,14 @@
-from .executor import ExecutionError, Executor
+from .executor import ConcurrentUpdateError, ExecutionError, Executor, StepLimitExceeded
 from .graph import Graph, NodeSpec
+from .reducers import concat, merge_dicts
 
-__all__ = ["ExecutionError", "Executor", "Graph", "NodeSpec"]
+__all__ = [
+    "ConcurrentUpdateError",
+    "ExecutionError",
+    "Executor",
+    "Graph",
+    "NodeSpec",
+    "StepLimitExceeded",
+    "concat",
+    "merge_dicts",
+]
