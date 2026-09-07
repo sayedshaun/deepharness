e7509ab319c41ef83767582466b7dd3b99486fd5
Sayed Shaun <sayedshaun4@gmail.com>
2026-09-07 22:44:31 +0600

    feat: export TavilySearch and SearchResult

--- deepharness/tools/__init__.py
+++ deepharness/tools/__init__.py
@@ -1,3 +1,13 @@
+from .tavily import SearchResult, TavilySearch, format_results
 from .toolbox import Ctx, Toolbox, ToolSpec, json_type, tool
 
-__all__ = ["Ctx", "ToolSpec", "Toolbox", "json_type", "tool"]
+__all__ = [
+    "Ctx",
+    "SearchResult",
+    "TavilySearch",
+    "ToolSpec",
+    "Toolbox",
+    "format_results",
+    "json_type",
+    "tool",
+]
